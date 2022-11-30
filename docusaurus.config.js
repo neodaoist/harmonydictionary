5110c67d21dfa366b5af6b149f77f85847957fa4
neodaoist <tom@loudverse.com>
2022-11-29 22:29:25 -0500

    :wrench: Tweak GH pages config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Harmony Dictionary',
   tagline: 'a site with helpful information on tonal harmony',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://neodaoist.github.io/',  
+  baseUrl: '/harmonydictionary/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -18,6 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'neodaoist', // Usually your GitHub org/user name.
   projectName: 'harmonydictionary', // Usually your repo name.
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
